e05bdfff38a9057ee764de4fae862523272d6a59
Ubuntu <ubuntu@oslo-training-28.c.praqma-training.internal>
2019-08-08 08:51:22 +0000

    Modified

--- github_issue.py
+++ github_issue.py
@@ -4,12 +4,14 @@ import requests
 
 # Authentication for user filing issue (must have read/write access to
 # repository to add issue to)
-USERNAME = 'YourGitHubHandle'
+USERNAME = 'nimaeivazzadeh'
 PASSWORD = os.environ['GITHUB_PASSWORD']
 
+
 # The repository to add this issue to
-REPO_OWNER = 'YourGitHubHandle'
+REPO_OWNER = 'Mohamad-Sobhie'
 REPO_NAME = 'ca-project'
+
 
 def make_github_issue(title, body=None, labels=None):
     '''Create an issue on github.com using the given parameters.'''
